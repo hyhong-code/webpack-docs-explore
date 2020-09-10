bb30b6976403a8cf0607a100ca52b6bf743e09c1
Haiyang Hong <hong961127@gmail.com>
2020-09-10 12:32:22 -0700

    Add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,34 @@ const path = require("path");
 
 module.exports = {
   mode: "development", // dev or prod
+
   // devtool: "none", // disable eval()
+
   entry: "./src/index.js",
+
   output: {
     filename: "main.js",
     // Resolve an absolute path to ./dist
     path: path.resolve(__dirname, "dist"),
   },
+
   module: {
     rules: [
+      // css config
+      // {
+      //   test: /\.css$/,
+      //   // Translate css to js first, then apply style to DOM
+      //   use: ["style-loader", "css-loader"], // Array order is in reverse ***
+      // },
+
+      // SCSS config
       {
-        test: /\.css$/,
-        // Translate css to js first, then apply style to DOM
-        use: ["style-loader", "css-loader"], // Array order is in reverse ***
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. Inject styles into DOM
+          "css-loader", // 2. css -> js
+          "sass-loader", // 1. scss -> css
+        ],
       },
     ],
   },
